013219bd9b3fc3cbca5b79e1e69d2a5895363231
Oliver Wright <bwright52@my.bcit.ca>
2026-05-06 13:56:25 -0700

    Adjust buyListings.js to display listing cards dynamically from the database

--- src/scripts/buyListings.js
+++ src/scripts/buyListings.js
@@ -1,14 +1,18 @@
-function displayListings(){
+document.addEventListener("DOMContentLoaded", async() => {
+    const response = await fetch(`http://localhost:3000/loadListings`)
+    const data = await response.json()
+    // console.log(data)
     listingHolder = document.getElementById("listingsHolder")
-    for (let i = 0; i < 7; i++){
+    data.forEach((newListing) => {
+        console.log(newListing)
         let listingHTML = ""
         listingHTML += `
         <div class="mx-auto md:mx-0 flex flex-col rounded-2xl drop-shadow-xl border-2 border-gray-300 w-88 h-96 bg-white">
-                <img src="/images/pantry_share_img_07.jpg" class="rounded-t-2xl h-3/5">
+                <img src="${newListing.image}" class="rounded-t-2xl h-3/5">
                 <div class="flex flex-col gap-12 py-2 px-10">
                     <div>
-                        <h2 class="font-bold text-xl">Whole Foods Bundle</h2>
-                        <h4 class="text-orange font-semibold">$0.00</h4>
+                        <h2 class="font-bold text-xl">${newListing.title}</h2>
+                        <h4 class="text-orange font-semibold">$${newListing.price}</h4>
                     </div>
                     <div class="flex justify-between gap-2">
                         <button class="bg-orange rounded-xl py-2 mb-4 text-white font-semibold cursor-pointer">
@@ -25,7 +29,5 @@ function displayListings(){
         listingDiv = document.createElement("div")
         listingDiv.innerHTML = listingHTML
         listingHolder.appendChild(listingDiv)
-    }
-}
-
-displayListings()
+    })
+})
