08bdcab558445a374494be2293de0d2a39ea3f65
Alex Harris <alexharris@live.co.uk>
2026-05-07 18:40:59 -0700

    Update UI on tutorial pop-ups

--- src/scripts/tutorialCreate.js
+++ src/scripts/tutorialCreate.js
@@ -1,16 +1,16 @@
 //Display tutorial
 const createTutorial = async function() {
     const currentUser = await fetch("/user")
-    if(!currentUser.tutorials.create) {
+    if(true) {
 
         //Display tutorial
         let tutorialContainer = document.createElement("div");
         tutorialContainer.classList.add("tutorialContainer", "absolute", "bg-black/40", "flex", "top-0", "left-0", "px-10", "h-screen", "w-full", "z-25", "items-center", "justify-center");
 
         let stepOne = `
-            <div id="stepOne" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-60 justify-center shadow-xl">
+            <div id="stepOne" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-70 justify-center shadow-xl">
                 <button class="closeButton absolute top-2 right-2 rounded-full w-7 h-7 bg-[#D9D9D9]"><img src="images/closeIcon.png" class="p-2"></button>
-                <img src="images/tutorialCreateStep1.png" class="mb-5">
+                <img src="images/tutorialCreateStep1.png" class="mb-5 p-2">
                 <h2 class="text-lg text-bold text-center font-bold mb-5  leading-[1.1]">Upload a photo and enter listing information</h2>
                 <button class="nextButtonTwo bg-[#FF6700] rounded-lg px-4 py-2 mb-5 text-white font-bold">Next</button>
                 <div class="progressBar flex justify-center gap-2">
@@ -22,9 +22,9 @@ const createTutorial = async function() {
         `
 
         let stepTwo = `
-            <div id="stepTwo" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-60 justify-center shadow-xl hidden">
+            <div id="stepTwo" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-70 justify-center shadow-xl hidden">
                 <button class="closeButton absolute top-2 right-2 rounded-full w-7 h-7 bg-[#D9D9D9]"><img src="images/closeIcon.png" class="p-2"></button>
-                <img src="images/tutorialCreateStep2.png" class="mb-5">
+                <img src="images/tutorialCreateStep2.png" class="mb-5 p-2">
                 <h2 class="text-lg text-bold text-center font-bold mb-5  leading-[1.1]">Add food contents and select categories</h2>
                 <button class="nextButtonThree bg-[#FF6700] rounded-lg px-4 py-2 mb-5 text-white font-bold">Next</button>
                 <div class="progressBar flex justify-center gap-2">
@@ -36,9 +36,9 @@ const createTutorial = async function() {
         `
 
         let stepThree = `
-            <div id="stepThree" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-60 justify-center shadow-xl hidden">
+            <div id="stepThree" class="relative flex flex-col bg-white rounded-lg pb-5 px-10 pt-10 max-w-70 justify-center shadow-xl hidden">
                 <button class="closeButton absolute top-2 right-2 rounded-full w-7 h-7 bg-[#D9D9D9]"><img src="images/closeIcon.png" class="p-2"></button>
-                <img src="images/tutorialCreateStep3.png" class="mb-5">
+                <img src="images/tutorialCreateStep3.png" class="mb-5 p-2">
                 <h2 class="text-lg text-bold text-center font-bold mb-5  leading-[1.1]">Click create when you are ready to publish</h2>
                 <button class="closeButton bg-[#FF6700] rounded-lg px-4 py-2 mb-5 text-white font-bold">Close</button>
                 <div class="progressBar flex justify-center gap-2">
