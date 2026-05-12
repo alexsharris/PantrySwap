c23ec75d357aec15b36be9950f3622ec0d6cb1fe
Oliver Wright <bwright52.my.bcit.ca>
2026-05-12 11:14:35 -0700

    Force listings without valid images to use placeholder images for listing cards

--- src/scripts/buyListings.js
+++ src/scripts/buyListings.js
@@ -6,9 +6,10 @@ document.addEventListener("DOMContentLoaded", async() => {
     data.forEach((newListing) => {
         console.log(newListing)
         let listingHTML = ""
+        const placeholderImg = "images/pantry_share_img_10.jpg"
         listingHTML += `
         <div class="mx-auto md:mx-0 flex flex-col rounded-2xl drop-shadow-xl border-2 border-gray-300 w-88 h-96 bg-white">
-                <img src="${newListing.image}" class="rounded-t-2xl h-3/5">
+                <img src="${(newListing.image)?newListing.image: placeholderImg}" class="rounded-t-2xl h-3/5">
                 <div class="flex flex-col gap-12 py-2 px-10">
                     <div>
                         <h2 class="font-bold text-xl">${newListing.title}</h2>
