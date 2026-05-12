5d1fc9815f552a1b354934bb77d4aa650f824968
Rayne Choy <echoy5@my.bcit.ca>
2026-05-11 17:18:19 -0700

    Add navbar navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,7 @@ class Navbar extends HTMLElement {
   constructor() {
     super();
     this.renderNavbar();
+    this.addEventListeners();
   }
 
   renderNavbar() {
@@ -19,16 +20,16 @@ class Navbar extends HTMLElement {
             <div class="flex justify-end gap-3">
                 <div class="flex gap-12 self-center">
                     <div class="flex gap-6 md:visible md:w-auto w-0 invisible">
-                        <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="buyBtnDesktop">
+                        <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="buyBtnDesktop">
                         Buy
                         </button>
-                        <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="sellBtnDesktop">
+                        <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="sellBtnDesktop">
                         Sell
                         </button>
-                        <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="savedBtnDesktop">
+                        <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="savedBtnDesktop">
                         Saved
                         </button>
-                        <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="accountBtnDesktop">
+                        <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="accountBtnDesktop">
                         Account
                         </button>
                 
@@ -39,24 +40,49 @@ class Navbar extends HTMLElement {
         </div>
 
         <div class="flex w-screen justify-between gap-2 visible md:hidden fixed inset-x-0 bottom-0 right-0 left-0 bg-white md:relative h-16 p-2 ">
-            <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="buyBtnDesktop">
+            <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="buyBtnDesktop">
             Buy
             </button>
-            <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="sellBtnDesktop">
+            <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="sellBtnDesktop">
             Sell
             </button>
-            <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="savedBtnDesktop">
+            <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="savedBtnDesktop">
             Saved
             </button>
-            <button class="hover:bg-[#F6E8E8] text-[#9B9B9B] hover:text-[#FF6700] rounded-lg p-2 min-w-18" id="accountBtnDesktop">
+            <button class="box-color-5 hover:text-orange hover:bg-peach rounded-lg p-2 min-w-18" id="accountBtnDesktop">
             Account
             </button>
         </div>
     </nav>
     `;
   }
-}
 
-// bg-[#FF6700]
+  addEventListeners() {
+    const navigateIfNeeded = (path) => {
+      if (window.location.pathname.toLowerCase() !== path.toLowerCase()) {
+        window.location.href = path;
+      }
+    };
+    // BUY
+    this.querySelector("#buyBtnDesktop")?.addEventListener("click", () => {
+      navigateIfNeeded("/buy");
+    });
+
+    // SELL
+    this.querySelector("#sellBtnDesktop")?.addEventListener("click", () => {
+      navigateIfNeeded("/sell");
+    });
+
+    // SAVED
+    this.querySelector("#savedBtnDesktop")?.addEventListener("click", () => {
+      navigateIfNeeded("/bookmark");
+    });
+
+    // ACCOUNT
+    this.querySelector("#accountBtnDesktop")?.addEventListener("click", () => {
+      navigateIfNeeded("/Account");
+    });
+  }
+}
 
 customElements.define("site-navbar", Navbar);
