501f2aeb73e0d3656757ed8d30d0bb7b4e2d3fc7
rayne choy <echoy5@my.bcit.ca>
2026-05-20 08:14:40 -0700

    Dynamic load image and text for tutorials

--- src/scripts/tutorialSystem.js
+++ src/scripts/tutorialSystem.js
@@ -1,8 +1,10 @@
+// =================================================
+// Imports + variables
+// =================================================
 import { closePopupWindow, displayWindow } from "./popupWindow.js";
+import { tutorialData } from "./tutorials.js";
 
-let tutorialState = null;
-
-const buttons = [
+const popupWindowButtons = [
   {
     label: "Next",
     color: "box-color-0",
@@ -20,7 +22,17 @@ const buttons = [
   },
 ];
 
-async function getTutorialState() {
+let tutorialState = [];
+let currentTutorial = null;
+let currentPage = 0;
+
+let tutorialImage = null;
+let tutorialText = null;
+let tutorialProgressBar = null;
+// =================================================
+// Logic
+// =================================================
+async function getUserTutorialState() {
   try {
     const response = await fetch("/user");
     const currentUser = await response.json();
@@ -29,37 +41,21 @@ async function getTutorialState() {
     console.log(e);
   }
 }
+// Get the data associated with a tutorial's steps
+const getTutorialData = (name) => tutorialData.find((tut) => tut.name == name);
 
 function nextStepButtonEvent() {
-  console.log("NEXT BUTTON");
+  currentPage++;
+  if (currentTutorial["steps"].length <= currentPage) closePopupWindow();
+  else renderTutorialPage();
 }
+// =================================================
+// Rendering
+// =================================================
 
-function callTutorial(tutorialName) {
-  const tutorialWindowMainContent = `
-  <div class="flex flex-col gap-3 items-center justify-center">
-    <img src="images/tutorialSearchStep1.png" class="size-50 object-scale-down">
-    <p class="font-semibold">Select the bookmark button to add to your collectionSelect the bookmark button to add to your collectionSelect the bookmark button to add to your collectionSelect the bookmark button to add to your collectionSelect the bookmark button to add to your collection</p>
-  </div>`;
-
-  displayWindow(
-    tutorialWindowMainContent,
-    buttons,
-    true,
-    "card flex flex-col items-center justify-center md:w-1/3 w-1/2 text-center",
-    null,
-    "flex w-full flex-1 pt-5",
-  );
-
-  const popupCard = document.getElementById("popup-card");
-  popupCard.insertAdjacentHTML(
-    "beforeend",
-    `<div id = "tutorial-step" class="flex justify-center gap-2 p-4">
-                <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
-                <div class="bg-[#FF6700] rounded-full h-2 w-2"></div>
-                <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
-                <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
-            </div>`,
-  );
+function renderTutorialPage() {
+  tutorialImage.src = currentTutorial["steps"][currentPage].image;
+  tutorialText.textContent = currentTutorial["steps"][currentPage].text;
   //   Set tutorial to completed
   //   const res = await fetch(
   //       `/updateUser/${currentUser._id}`,
@@ -75,4 +71,44 @@ function callTutorial(tutorialName) {
   //     );
 }
 
-document.addEventListener("load", callTutorial("test"));
+async function callTutorial(tutorialName) {
+  await getUserTutorialState();
+  // if (!tutorialState || tutorialState.includes(tutorialName)) return;
+
+  currentTutorial = getTutorialData(tutorialName);
+  if (!currentTutorial) return;
+
+  const tutorialWindowMainContent = `
+  <div class="flex flex-col gap-3 items-center justify-center">
+    <img id="tutorial-img" src="${getTutorialData("create")["steps"][1].image}" class="size-50 object-scale-down">
+    <p id="tutorial-text" class="font-semibold">${getTutorialData("create")["steps"][0].text}</p>
+  </div>`;
+
+  displayWindow(
+    tutorialWindowMainContent,
+    popupWindowButtons,
+    false,
+    "card flex flex-col items-center justify-center md:w-1/3 w-1/2 text-center",
+    null,
+    "flex w-full flex-1 pt-5",
+  );
+
+  const popupCard = document.getElementById("popup-card");
+  popupCard.insertAdjacentHTML(
+    "beforeend",
+    `<div id = "tutorial-step" class="flex justify-center gap-2 p-4">
+    <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
+    <div class="bg-[#FF6700] rounded-full h-2 w-2"></div>
+    <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
+    <div class="bg-[#D9D9D9] rounded-full h-2 w-2"></div>
+    </div>`,
+  );
+
+  tutorialProgressBar = document.getElementById("popup-card");
+  tutorialImage = document.getElementById("tutorial-img");
+  tutorialText = document.getElementById("tutorial-text");
+
+  renderTutorialPage(currentPage);
+}
+
+document.addEventListener("load", callTutorial("create"));
---
+++ src/scripts/tutorials.js
@@ -0,0 +1,23 @@
+export const tutorialData = [
+  {
+    name: "create",
+    steps: [
+      {
+        image: "images/tutorialSearchStep1.png",
+        text: "Choose from a a range of food categories",
+      },
+      {
+        image: "images/tutorialSearchStep2.png",
+        text: "or, filter by listings nearest to you",
+      },
+      {
+        image: "images/tutorialBookmarkStep1.png",
+        text: "Select the bookmark button to add to your collection",
+      },
+      {
+        image: "images/tutorialBookmarkStep2.png",
+        text: "View bookmarks in the saved tab",
+      },
+    ],
+  },
+];
