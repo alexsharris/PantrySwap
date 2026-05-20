f7fbec688d9f6d599b5e4f42ea213b4722567a6c
Saba_Zakeri_Far <szakerifar@my.bcit.ca>
2026-05-20 13:53:46 -0700

    Add empty state card for sell page

--- src/scripts/sellerListings.js
+++ src/scripts/sellerListings.js
@@ -25,6 +25,14 @@ displaySellerListings = async function () {
       );
       listingHolder.appendChild(newCard);
     });
+
+    //children gives a live collection elements we can count with length
+    // if no child is appended, reveal the empty state
+    if (listingHolder.children.length === 0) {
+      const banner = document.getElementById("emptyStateBanner");
+      banner.classList.remove("hidden");
+      banner.classList.add("flex");
+    }
   } catch (error) {
     console.error(error);
   }
