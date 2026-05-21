2e99294323176f39dbc25c007f695f675295435f
Flo <227476052+flo-in-code@users.noreply.github.com>
2026-05-21 10:54:18 -0700

    Update port in app.listen

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ async function main() {
     .connect(db)
     .then(() => {
       console.log("Connected to MongoDB");
-      app.listen(3000, () => {
+      app.listen(port, () => {
         console.log("Server running on port 3000");
       });
     })
